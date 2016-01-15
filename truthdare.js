1b254cf6de5f79782c28790f2bbfa31b9e287b86
klee4017 <krysten@moeliew.com>
2016-01-15 04:07:47 +0000

    added pictures and added more stuff for the truth or dare stuff.

--- truthdare.js
+++ truthdare.js
@@ -1,14 +1,34 @@
 // JavaScript File
-var button = document.getElementById("Truth")
-var button = document.getElementById("Dare")
 var truth = "truth";
 var dare = "dare";
 
 
 
+var displayTruth = document.getElementById('displayTruth');
+var displayDare = document.getElementById('displayDare');
 
 
 
+function generateTruth (){
+    var truthArray = [
+    "How many people have you dated?" ,
+    "Would you rather spend a whole day with your favorite singer or a week with your crush?",
+  ];
+    var truthResult = truthArray[Math.floor(Math.random() * truth.length)];
+    displayTruth.innerHTML = truthResult;
+    generateTruth();
+}
+
+function generateDare (){
+    var dareArray = [
+    "Tim" ,
+    "John",
+    "Sam"
+  ];
+    var dareResult = dareArray[Math.floor(Math.random() * dareArray.length)];
+    displayDare.innerHTML = dareResult;
+    generateDare();
+}
 
 
 
