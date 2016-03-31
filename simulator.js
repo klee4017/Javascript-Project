cb3596e822955ce6ff3265191af0af0e25dee4bf
klee4017 <krysten@moeliew.com>
2016-03-31 21:20:44 +0000

    added stuff for simulator

--- simulator.js
+++ simulator.js
@@ -1,1 +1,13 @@
-// JavaScript File
+ // JavaScript File
+ var buttonOne = document.getElementById("buttonOne");
+ var buttonTwo = document.getElementById("buttonTwo");
+ var buttonThree = document.getElementById("buttonThree");
+ var buttonFour = document.getElementById("buttonFour");
+ var buttonFive = document.getElementById("buttonFive");
+ var theChanger;
+ 
+buttonOne.addEventListener("click", function(){
+ theChanger.style.
+ 
+})
+ 
