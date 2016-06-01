9fd5d215ad66b9082c526b8fb639a1282d70dcb4
klee4017 <krysten@moeliew.com>
2016-06-01 22:08:43 +0000

    added a few funcitoning buttons to the simulator.

--- simulator.js
+++ simulator.js
@@ -1,13 +1,46 @@
  // JavaScript File
+ var theChanger = document.getElementById("theChanger");
+ var backgroundColor = document.getElementById("background")
  var buttonOne = document.getElementById("buttonOne");
  var buttonTwo = document.getElementById("buttonTwo");
  var buttonThree = document.getElementById("buttonThree");
  var buttonFour = document.getElementById("buttonFour");
  var buttonFive = document.getElementById("buttonFive");
- var theChanger;
+ var buttonSix = document.getElementById("buttonSix");
+ var buttonSeven = document.getElementById("buttonSeven");
+ var buttonEight = document.getElementById("buttonEight");
+ var buttonNine = document.getElementById("buttonNine");
+ var buttonTen = document.getElementById("buttonTen");
  
 buttonOne.addEventListener("click", function(){
- theChanger.style.
- 
-})
- 
+ theChanger.style.color = "red" ;
+});
+buttonTwo.addEventListener("click", function(){
+ theChanger.style.color = "blue" ;
+});
+buttonThree.addEventListener("click", function(){
+ theChanger.style.color = "green" ;
+});
+buttonFour.addEventListener("click", function(){
+ theChanger.style.color = "magenta" ;
+});
+buttonFive.addEventListener("click", function(){
+ theChanger.style.color = "white" ;
+});
+
+
+buttonSix.addEventListener("click", function(){
+ theChanger.style.color = "300px green";
+});
+buttonSeven.addEventListener("click", function(){
+ theChanger.style.color = "blue" ;
+});
+buttonEight.addEventListener("click", function(){
+ theChanger.style.color = "green" ;
+});
+buttonNine.addEventListener("click", function(){
+ theChanger.style.color = "magenta" ;
+});
+buttonTen.addEventListener("click", function(){
+ theChanger.style.color = "white" ;
+});
